12f1888af276c596e8ac09ac491ec7547f2c95ed
Hamza Feroze <hamzaferoze115@gmail.com>
2024-05-24 21:07:17 +0000

    Created the map server in sim and real robot.TASK 1- Mapping.

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -1,21 +1,31 @@
 import os
-from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-def generate_launch_description():
+def launch_setup(command, *args, **kwargs):
+    mode = command.launch_configurations['mode']
 
     cartographer_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'config')
-    configuration_basename = 'cartographer.lua'
 
-    return LaunchDescription([
-        
+    if mode == 'real':
+        configuration_basename = 'realcartographer.lua'
+        rviz_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'config', 'real_cartographer.rviz')
+        use_sim_time = False
+    else:
+        configuration_basename = 'cartographer.lua'
+        rviz_config_dir = os.path.join(get_package_share_directory('cartographer_slam'), 'config', 'sim_cartographer.rviz')
+        use_sim_time = True
+
+    return [
         Node(
             package='cartographer_ros', 
             executable='cartographer_node', 
             name='cartographer_node',
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-configuration_directory', cartographer_config_dir,
                        '-configuration_basename', configuration_basename]),
 
@@ -24,7 +34,23 @@ def generate_launch_description():
             executable='cartographer_occupancy_grid_node',
             output='screen',
             name='occupancy_grid_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-resolution', '0.05', '-publish_period_sec', '1.0']
         ),
-    ]) 
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz_node',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-d', rviz_config_dir])
+    ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'mode',
+            default_value='real',
+            description='Simulation or real mode.'),
+        OpaqueFunction(function=launch_setup)
+    ])
---
+++ map_server/launch/map_server.launch.py
@@ -0,0 +1,55 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def launch_setup(command, *args, **kwargs):
+    map_file = command.launch_configurations['map_file']
+
+    
+    map_path = os.path.join(get_package_share_directory('map_server'), 'config', map_file)
+
+    if map_file == 'warehouse_map_real.yaml':
+        rviz_config_dir = os.path.join(get_package_share_directory('map_server'), 'config', 'real_map.rviz')
+        use_sim_time = False
+    else:
+        rviz_config_dir = os.path.join(get_package_share_directory('map_server'), 'config', 'sim_map.rviz')
+        use_sim_time = True
+
+    return [
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time}, 
+                        {'yaml_filename':map_path} 
+                       ]),
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_mapper',
+            output='screen',
+            parameters=[{'use_sim_time': use_sim_time},
+                        {'autostart': True},
+                        {'node_names': ['map_server']}]),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz_node',
+            parameters=[{'use_sim_time': use_sim_time}],
+            arguments=['-d', rviz_config_dir])
+    ]
+
+def generate_launch_description():
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'map_file',
+            default_value='warehouse_map_real.yaml',
+            description='simulation or real robot map.'),
+        OpaqueFunction(function=launch_setup)
+    ])
